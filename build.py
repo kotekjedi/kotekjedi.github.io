c0ddfd4921a95bfcb72f4f3672a05d57bb49844e
Alexander Panfilov <sasha_pusha@mail.de>
2024-10-16 19:57:30 +0200

    goat counter

--- build.py
+++ build.py
@@ -261,6 +261,8 @@ def get_index_html():
 <!-- Bootstrap JS -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
+<script data-goatcounter="https://kotekjedi.goatcounter.com/count"
+        async src="//gc.zgo.at/count.js"></script>
 </head>
 
 
