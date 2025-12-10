30736be286e84c02011f736e07e80740bf5b336e
Alexander Panfilov <sasha_pusha@mail.de>
2025-12-10 16:15:04 +0100

    upd

--- build.py
+++ build.py
@@ -398,7 +398,7 @@ def get_index_html() -> str:
         <head>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>{PERSON["first_name"]} {PERSON["last_name"]} - AI Safety Research</title>
+            <title>{PERSON["first_name"]} {PERSON["last_name"]}</title>
             <meta name="description" content="{PERSON["tagline"]}">
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
